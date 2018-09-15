04dc9a9f5e627d24547b3e0ef78e5a0b8e0ea852
NguyenTuAnh <anhnt01@gemvietnam.com>
2018-09-15 22:37:52 +0700

    first commit

--- PedometerDemo/ViewController.swift
+++ PedometerDemo/ViewController.swift
@@ -7,19 +7,104 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stepsCountLabel: UILabel!
+    @IBOutlet weak var activityTypeLabel: UILabel!
+
+    private let activityManager = CMMotionActivityManager()
+    private let pedometer = CMPedometer()
+    private var shouldStartUpdating: Bool = false
+    private var startDate: Date? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let startDate = startDate else { return }
+        updateStepsCountLabelUsing(startDate: startDate)
+    }
+    
+    @IBAction func startAndStopAction(_ sender: UIButton) {
+        shouldStartUpdating = !shouldStartUpdating
+        shouldStartUpdating ? (onStart()) : (onStop())
+    }
+
+}
+
+extension ViewController {
+
+    private func onStart() {
+        startButton.setTitle("Stop", for: .normal)
+        startDate = Date()
+        startUpdating()
+    }
+
+    private func onStop() {
+        startButton.setTitle("Start", for: .normal)
+        startDate = nil
+        stopUpdating()
+    }
+
+    private func startUpdating() {
+        if CMMotionActivityManager.isActivityAvailable() {
+            startTrackingActivityType()
+        }
+        if CMPedometer.isStepCountingAvailable() {
+            startCountingSteps()
+        }
+    }
+
+    private func stopUpdating() {
+        activityManager.stopActivityUpdates()
+        pedometer.stopUpdates()
+        pedometer.stopEventUpdates()
     }
 
 
+    private func updateStepsCountLabelUsing(startDate: Date) {
+        pedometer.queryPedometerData(from: startDate, to: Date()) {
+            [weak self] pedometerData, error in
+            if error != nil, let pedometerData = pedometerData {
+                DispatchQueue.main.async {
+                    self?.stepsCountLabel.text = String(describing: pedometerData.numberOfSteps)
+                }
+            }
+        }
+    }
+
+    private func startTrackingActivityType() {
+        activityManager.startActivityUpdates(to: OperationQueue.main) {
+            [weak self] (activity: CMMotionActivity?) in
+            guard let activity = activity else { return }
+            DispatchQueue.main.async {
+                if activity.walking {
+                    self?.activityTypeLabel.text = "Đi dạo"
+                } else if activity.stationary {
+                    self?.activityTypeLabel.text = "Đứng yên"
+                } else if activity.running {
+                    self?.activityTypeLabel.text = "Chạy"
+                } else if activity.automotive {
+                    self?.activityTypeLabel.text = "Ô tô"
+                }
+            }
+        }
+    }
+
+    private func startCountingSteps() {
+        pedometer.startUpdates(from: Date()) {
+            [weak self] pedometerData, error in
+            guard let pedometerData = pedometerData, error == nil else { return }
+
+            DispatchQueue.main.async {
+                self?.stepsCountLabel.text = pedometerData.numberOfSteps.stringValue
+            }
+        }
+    }
 }
 
